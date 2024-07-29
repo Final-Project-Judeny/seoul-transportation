bac5abf360a5f67802a7ac510b1ae6d6fc602a8e
SangwonJwa <jwa4610@gmail.com>
2024-07-30 02:37:42 +0900

    schedule 조정

--- airflow/dags/leisure_sports_to_s3.py
+++ airflow/dags/leisure_sports_to_s3.py
@@ -86,7 +86,7 @@ def fetch_and_upload_leisure_sports(bucket_name, object_name, execution_date, **
 with DAG(
     dag_id="s3_upload_leisure_sports",
     start_date=datetime(2024, 7, 28),
-    schedule_interval="0 11 * * 2",
+    schedule_interval='0 2 * * 2',
     catchup=False,
     default_args={
         "retires" : 1,
