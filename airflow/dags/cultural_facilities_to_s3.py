dccf6c64ab0e8ec4e510e8f868afa9c6ec2aef9f
SangwonJwa <jwa4610@gmail.com>
2024-07-29 20:01:54 +0900

    DAG 의존성 추가

--- airflow/dags/cultural_facilities_to_s3.py
+++ airflow/dags/cultural_facilities_to_s3.py
@@ -7,6 +7,7 @@ import pandas as pd
 import json
 import io
 import pytz
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 # S3에서 파일을 다운로드하고 DataFrame으로 로드하는 함수
 def load_csv_from_s3(bucket_name, object_name):
@@ -103,6 +104,14 @@ with DAG(
             "execution_date": "{{ ds }}",
         },
         provide_context=True,
+    )
+    
+    trigger_check_dag_task = TriggerDagRunOperator(
+        task_id='trigger_check_dag_cultural_facilities',
+        trigger_dag_id='s3_upload_check',
+        execution_date='{{ ds }}',
+        conf={'task': 'empty_task_cultural_facilities'},
+        wait_for_completion=False
     )
 
 fetch_and_upload_cultural_facilities_task
