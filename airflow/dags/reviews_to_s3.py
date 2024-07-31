34536b0b0e3666d6bab5871c2d71e90e6a3ea712
문송은 <munsong-eun@munsong-eun-ui-MacBookAir.local>
2024-07-31 17:48:48 +0900

    리뷰 생성 dag 추가

---
+++ airflow/dags/reviews_to_s3.py
@@ -0,0 +1,147 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from ReviewDataGenerator import *
+from io import StringIO
+
+import pandas as pd
+import json
+from datetime import datetime, timedelta
+import random as r
+from scipy.stats import skewnorm
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    's3_upload_reviews',
+    default_args=default_args,
+    description='Create random review data and upload to S3',
+    schedule_interval="0 11 * * 3",
+    start_date=datetime(2024, 7, 1),
+    catchup=False,
+) as dag:
+
+    def readTour(file, base_key, bucket_name, **kwargs):
+        task_instance = kwargs['ti']
+
+        # S3 연결
+        hook = S3Hook('aws_conn_id')
+
+        # tour 정보 로드
+        try:
+            key = f"{base_key}/{file}/{file}/{file}.csv"
+            tour_content = hook.read_key(key=key, bucket_name=bucket_name)
+            tour_info = pd.read_csv(StringIO(tour_content))
+            filtered_tour_info = tour_info['contentid'].unique().tolist()
+            task_instance.log.info("Successfully read csv file.")
+        except Exception as e:
+            task_instance.log.error(f"Error occurred while read csv file: {e}") 
+            raise
+
+        task_instance.xcom_push(key='tour_info', value=filtered_tour_info)
+
+    def createReviews(**kwargs):
+        task_instance = kwargs['ti']
+        cf = task_instance.xcom_pull(key='tour_info', task_ids='read_cf_info') or [] # cultural_facilities
+        fs = task_instance.xcom_pull(key='tour_info', task_ids='read_fs_info') or [] # festivals
+        ls = task_instance.xcom_pull(key='tour_info', task_ids='read_ls_info') or [] # leisure_sports
+        ts = task_instance.xcom_pull(key='tour_info', task_ids='read_ts_info') or [] # tourist_spots
+        
+        all_tour_id = cf+fs+ls+ts
+        try:
+            reviews = ReviewDataGenerator(all_tour_id)
+        except Exception as e:
+            task_instance.log.error(f'Error occurred while creating review data: {e}')
+        
+        task_instance.xcom_push(key='reviews', value=reviews)
+
+
+    def uploadToS3(base_key, bucket_name, execution_date, **kwargs):
+        task_instance = kwargs['ti']
+        reviews = task_instance.xcom_pull(key='reviews', task_ids='createReviews') or []
+
+        # S3 연결
+        hook = S3Hook('aws_conn_id')
+
+        # S3에 적재 (json)
+        try:
+            result_json = json.dumps(reviews, ensure_ascii=False, indent=4)
+            json_file_name = f"관광지_리뷰_{execution_date}.json"
+            json_key = f"{base_key}/reviews/{json_file_name}"
+            hook.load_string(
+                string_data=result_json,
+                key=json_key,
+                bucket_name=bucket_name,
+                replace=True
+            )
+            task_instance.log.info(f"Successfully uploaded json file to S3.")
+        except Exception as e:
+            task_instance.log.error(f"Error occurred while uploading json file to S3: {e}")
+            raise
+        
+        task_instance.log.info(f"Done!")
+
+    readCF = PythonOperator(
+        task_id='read_cf_info',
+        python_callable=readTour,
+        op_kwargs={
+            'file': 'cultural_facilities', 
+            'base_key': 'tour',
+            'bucket_name': '{{ var.value.s3_bucket_name }}',
+        },
+    )
+
+    readFS = PythonOperator(
+        task_id='read_fs_info',
+        python_callable=readTour,
+        op_kwargs={
+            'file': 'festivals',
+            'base_key': 'tour',
+            'bucket_name': '{{ var.value.s3_bucket_name }}',
+        },
+    )
+
+    readLS = PythonOperator(
+        task_id='read_ls_info',
+        python_callable=readTour,
+        op_kwargs={
+            'file': 'leisure_sports', 
+            'base_key': 'tour',
+            'bucket_name': '{{ var.value.s3_bucket_name }}',
+        },
+    )
+
+    readTS = PythonOperator(
+        task_id='read_ts_info',
+        python_callable=readTour,
+        op_kwargs={
+            'file': 'tourist_spots',
+            'base_key': 'tour',
+            'bucket_name': '{{ var.value.s3_bucket_name }}',
+        },
+    )
+
+    createReviews = PythonOperator(
+        task_id='create_review_data',
+        python_callable=createReviews,
+    )
+
+    uploadToS3 = PythonOperator(
+        task_id='upload_to_s3',
+        python_callable=uploadToS3,
+        op_kwargs={
+            'base_key': 'tour',
+            'bucket_name': '{{ var.value.s3_bucket_name }}',
+            "execution_date": "{{ ds }}",
+        }
+    )
+
+    # 작업 순서 정의
+    [readCF, readFS, readLS, readTS] >> createReviews >> uploadToS3
---
+++ airflow/plugins/ReviewDataGenerator.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import random as r
+from datetime import datetime
+from scipy.stats import skewnorm
+
+# curtural_facilities.csv, festivals.csv, leisure_sports.csv, tourist_spots.csv
+# 4개 파일의 contentsid를 받아 랜덤 리뷰 데이터를 생성해 반환하는 함수
+def ReviewDataGenerator(all_tour_id):
+    today_num_of_review = r.randint(500, 800)
+    review_df = pd.DataFrame(columns=['UserID', 'TouristSpotID', 'Timestamp', 'Score'])
+    for i in range(today_num_of_review):
+        # UserID
+        userid = r.randint(0,100000000)
+        
+        # TouristSpotID
+        tourid = r.choice(all_tour_id)
+        
+        # Timestamp
+        now = datetime.now()
+        year = now.year
+        month = now.month
+        day = now.day
+        hour = r.randint(0, 23)
+        minute = r.randint(0, 59)
+        second = r.randint(0, 59)
+        timestamp = datetime(year, month, day, hour, minute, second)
+        
+        # Score
+        score = int(skewnorm(a, loc, scale).rvs(1)[0])
+        if score > 100:
+            score = 100
+        elif score < 0:
+            score = 0
+
+        # 데이터 추가
+        review = pd.DataFrame({
+            'UserID': [userid],
+            'TouristSpotID': [tourid],
+            'Timestamp': [timestamp],
+            'Score': [score]
+        })
+        review_df = pd.concat([ review_df, review ])
+    
+    review_df = review_df.sort_values(by=['Timestamp'], axis=0).reset_index(drop=True)
+    return review_df
