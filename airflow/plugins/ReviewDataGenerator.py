7b933ca239b8829b0d7ff562b98afef6325aa6f4
문송은 <munsong-eun@munsong-eun-ui-MacBookAir.local>
2024-08-02 22:05:18 +0900

    logging 추가

--- airflow/plugins/ReviewDataGenerator.py
+++ airflow/plugins/ReviewDataGenerator.py
@@ -10,41 +10,44 @@ def ReviewDataGenerator(all_tour_data):
     today_num_of_review = r.randint(500, 800)
     review_df = pd.DataFrame(columns=['UserID', 'TouristSpotID', 'Title', 'Timestamp', 'Score', 'Category'])
     for i in range(today_num_of_review):
-        # UserID
-        userid = r.randint(0,100000000)
-        
-        # Randomly select a tour data (pair of contentid and title, and category)
-        tour_data = r.choice(all_tour_data)
-        tourid = tour_data['contentid']
-        title = tour_data['title']
-        category = tour_data['category']
-        
-        # Timestamp
-        now = datetime.now()
-        year = now.year
-        month = now.month
-        day = now.day
-        hour = r.randint(0, 23)
-        minute = r.randint(0, 59)
-        second = r.randint(0, 59)
-        logging.info(f"{year}-{month}-{day} {hour}:{minute}:{second}")
-        timestamp = datetime(year, month, day, hour, minute, second).strftime('%Y%m%d%H%M%S')
-        
-        # Score
-        a, loc, scale = 2, 50, 12
-        score = int(skewnorm(a, loc, scale).rvs(1)[0])
-        score = max(0, min(score, 100))
+        try:
+            # UserID
+            userid = r.randint(0,100000000)
+            
+            # Randomly select a tour data (pair of contentid and title, and category)
+            tour_data = r.choice(all_tour_data)
+            tourid = tour_data['contentid']
+            title = tour_data['title']
+            category = tour_data['category']
+            
+            # Timestamp
+            now = datetime.now()
+            year = now.year
+            month = now.month
+            day = now.day
+            hour = r.randint(0, 23)
+            minute = r.randint(0, 59)
+            second = r.randint(0, 59)
+            logging.info(f"{year}-{month}-{day} {hour}:{minute}:{second}")
+            timestamp = datetime(year, month, day, hour, minute, second).strftime('%Y%m%d%H%M%S')
+            
+            # Score
+            a, loc, scale = 2, 50, 12
+            score = int(skewnorm(a, loc, scale).rvs(1)[0])
+            score = max(0, min(score, 100))
 
-        # 데이터 추가
-        review = pd.DataFrame({
-            'UserID': [userid],
-            'TouristSpotID': [tourid],
-            'Title': [title],
-            'Timestamp': [timestamp],
-            'Score': [score],
-            'Category': [category]
-        })
-        review_df = pd.concat([review_df, review])
+            # 데이터 추가
+            review = pd.DataFrame({
+                'UserID': [userid],
+                'TouristSpotID': [tourid],
+                'Title': [title],
+                'Timestamp': [timestamp],
+                'Score': [score],
+                'Category': [category]
+            })
+            review_df = pd.concat([review_df, review])
+        except Exception as e:
+            logging.info(f"Error is occurred while creating reviews: {e}")
     
     review_df = review_df.sort_values(by=['Timestamp'], axis=0).reset_index(drop=True)
     logging.info(f"Review data was successfully created: {review_df[:10]}")
