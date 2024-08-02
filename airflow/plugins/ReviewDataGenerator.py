967f8197cf9f14418714c0965dfed991349319eb
문송은 <munsong-eun@munsong-eun-ui-MacBookAir.local>
2024-08-02 18:24:19 +0900

    category, title  컬럼 추가

--- airflow/plugins/ReviewDataGenerator.py
+++ airflow/plugins/ReviewDataGenerator.py
@@ -6,15 +6,18 @@ import logging
 
 # curtural_facilities.csv, festivals.csv, leisure_sports.csv, tourist_spots.csv
 # 4개 파일의 contentsid를 받아 랜덤 리뷰 데이터를 생성해 반환하는 함수
-def ReviewDataGenerator(all_tour_id):
+def ReviewDataGenerator(all_tour_data):
     today_num_of_review = r.randint(500, 800)
-    review_df = pd.DataFrame(columns=['UserID', 'TouristSpotID', 'Timestamp', 'Score'])
+    review_df = pd.DataFrame(columns=['UserID', 'TouristSpotID', 'Title', 'Timestamp', 'Score', 'Category'])
     for i in range(today_num_of_review):
         # UserID
         userid = r.randint(0,100000000)
         
-        # TouristSpotID
-        tourid = r.choice(all_tour_id)
+        # Randomly select a tour data (pair of contentid and title, and category)
+        tour_data = r.choice(all_tour_data)
+        tourid = tour_data['contentid']
+        title = tour_data['title']
+        category = tour_data['category']
         
         # Timestamp
         now = datetime.now()
@@ -30,19 +33,18 @@ def ReviewDataGenerator(all_tour_id):
         # Score
         a, loc, scale = 2, 50, 12
         score = int(skewnorm(a, loc, scale).rvs(1)[0])
-        if score > 100:
-            score = 100
-        elif score < 0:
-            score = 0
+        score = max(0, min(score, 100))
 
         # 데이터 추가
         review = pd.DataFrame({
             'UserID': [userid],
             'TouristSpotID': [tourid],
+            'Title': [title],
             'Timestamp': [timestamp],
-            'Score': [score]
+            'Score': [score],
+            'Category': [category]
         })
-        review_df = pd.concat([ review_df, review ])
+        review_df = pd.concat([review_df, review])
     
     review_df = review_df.sort_values(by=['Timestamp'], axis=0).reset_index(drop=True)
     logging.info(f"Review data was successfully created: {review_df[:10]}")
